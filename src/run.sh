a0721e7b04cf9ed76db838aedeb1422cbbf7c72f
Xinru Yan 🇨🇳  👑 <xinru1414@gmail.com>
2020-06-25 12:19:52 -0400

    updated primary aspects collection to take user input

--- src/run.sh
+++ src/run.sh
@@ -6,10 +6,14 @@ echo '#################################################################'
 echo 'Type in the brand (All for all brands)':
 read brand
 echo '#################################################################'
+echo Type in the product:
+read keyword
+echo '#################################################################'
 echo Building feng-hirst-rst-parser
 cd ../feng-hirst-rst-parser/
 docker build -t feng-hirst .
-# preprocess reviews
+echo '#################################################################'
+echo Preprocessing reviews.
 cd ../src/
 pipenv run python review_preprocess.py -r $input -o ../feng-hirst-rst-parser/preprocessed_texts/
 echo '#################################################################'
@@ -23,10 +27,18 @@ pipenv run python -m spacy download en_core_web_sm
 pipenv run python treeparser.py -i ../feng-hirst-rst-parser/results -o ../rst_results
 echo '#################################################################'
 echo Generating and examining primary aspects.
-pipenv run python primary_aspects.py -p ../rst_results/noun_pairs.pickle -na ../data/resources/not_cared_aspects -k mattress
+pipenv run python primary_aspects.py -p ../rst_results/noun_pairs.pickle -na ../data/resources/not_cared_aspects -k $keyword
+echo '#################################################################'
+echo Type in the selected primary aspects from the above aspect list, one aspect per line. Enter ctrl + D on a new line when finished.
+arrAspects=()
+while IFS= read -r aspect
+do
+    arrAspects+=("$aspect")
+done
+printf "%s\n" "${arrAspects[@]}" >> ../data/resources/primary_aspects
 echo '#################################################################'
 echo Generating and examining secondary aspects.
-pipenv run python aspect_hierarchy.py -b $brand -p ../feng-hirst-rst-parser/results -d $input -ca ../data/resources/primary_aspects -na ../data/resources/not_cared_aspects -rl ../data/resources/relations -dt ../data/resources/determiners -k mattress > "../data/brand/$brand.txt"
+pipenv run python aspect_hierarchy.py -b $brand -p ../feng-hirst-rst-parser/results -d $input -ca ../data/resources/primary_aspects -na ../data/resources/not_cared_aspects -rl ../data/resources/relations -dt ../data/resources/determiners -k $keyword > "../data/brand/$brand.txt"
 echo '#################################################################'
 echo Creating aspect hierarchy graphs.
 pipenv run python gen_graph.py -b $brand -o ../graphs/
